09268b1a6fc8b07928b85ef898638c2fd9f4a352
ethanbfc <ethanbfc@gmail.com>
2025-04-13 14:57:42 +0100

    Update compile and target SDK versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ethanmurray.footballhub"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ethanmurray.footballhub"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
